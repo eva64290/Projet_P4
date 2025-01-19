8fe4525c1a8ab056591a65f16afc736cc5aa207d
eva64290 <githubeva64290@hotmail.com>
2025-01-20 00:39:24 +0100

    main_controller.py

--- controllers/main_controller.py
+++ controllers/main_controller.py
@@ -1,4 +1,4 @@
-    from controllers.player_controller import PlayerController
+from controllers.player_controller import PlayerController
 from controllers.tournament_controller import TournamentController
 from views.menu_view import MenuView
 
